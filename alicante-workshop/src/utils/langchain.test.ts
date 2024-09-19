ae2b1d1ea3134ab8ae9f0d553c9a4551ed0d6e7b
shivay-at-pieces <shivay@pieces.app>
2024-09-19 11:09:07 +0530

    ex5

--- alicante-workshop/src/utils/langchain.test.ts
+++ alicante-workshop/src/utils/langchain.test.ts
@@ -3,11 +3,14 @@ import { generateAnswer } from './langchain';
 
 describe('LangChain', () => {
     it('Answers a question', async () => {
-       //         // 1. Add your own question here
+        const answer = await generateAnswer('United Kingdom', 'Is the following {question} a country, answer "yes" or "no" only.');
 
-        const answer = await generateAnswer('Is 2+2 = 4, answer "yes" or "no" only.');
-       //         // 2. Match the answer from the LLM to a predicted value
+        assert.equal(answer.trim(), "Yes");
+    });
 
-        assert.equal(answer.trim().toLowerCase(), "yes");
+    it('Answers a question incorrectly', async () => {
+        const answer = await generateAnswer('Amsterdam', 'Is the following {question} a country, answer "yes" or "no" only.');
+
+        assert.equal(answer.trim(), "no");
     });
 });
--- alicante-workshop/src/utils/langchain.ts
+++ alicante-workshop/src/utils/langchain.ts
@@ -1,18 +1,30 @@
 import { Ollama } from "@langchain/community/llms/ollama";
+import { PromptTemplate } from "@langchain/core/prompts";
 
 const llm = new Ollama({
     baseUrl: "http://localhost:11434",
     model: "llama3"
 });
 
-export async function generateAnswer(question: string) {
-    let answer = '';
+export async function generateAnswer(
+    question: string,
+    promptTemplate: string = "Take the role of a personal travel assistant, and answer the following question in detail: {question}?"
+) {
+    let answer = ''
+
+    const prompt = PromptTemplate.fromTemplate(
+        promptTemplate
+    );
+
+    const formattedPrompt = await prompt.format({
+        question,
+    });
 
     try {
-        answer = await llm.invoke(question);
+        answer = await llm.invoke(formattedPrompt);
     } catch (e) {
-        return 'Something went wrong';
+        return 'Something went wrong'
     }
 
-    return answer;
+    return answer
 }
