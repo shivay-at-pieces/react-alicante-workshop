f99527fb8a45455327e2b0cce308906371f73d25
shivay-at-pieces <shivay@pieces.app>
2024-09-19 10:40:48 +0530

    Update test

--- alicante-workshop/src/utils/langchain.test.ts
+++ alicante-workshop/src/utils/langchain.test.ts
@@ -5,7 +5,7 @@ describe('LangChain', () => {
     it('Answers a question', async () => {
        //         // 1. Add your own question here
 
-        const answer = await generateAnswer('Is United Kingdom a country, answer "yes" or "no" only.');
+        const answer = await generateAnswer('Is 2+2 = 4, answer "yes" or "no" only.');
        //         // 2. Match the answer from the LLM to a predicted value
 
         assert.equal(answer.trim().toLowerCase(), "yes");
